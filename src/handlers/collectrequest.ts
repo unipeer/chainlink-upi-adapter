21460b406b85deb202692ada5f058a00259eb3dc
Shaleen Jain <shaleen@jain.sh>
2020-08-20 13:09:05 +0530

    update response object for collect req

--- src/handlers/collectrequest.ts
+++ src/handlers/collectrequest.ts
@@ -21,17 +21,24 @@ export const collectRequestHandle = async (data: CollectRequest) => {
       .collectRequest(body)
       .then(result => result.json())
       .then((result: any) => {
-        return { statusCode: 201, data: {
-          txId: result.SeqNo,
-          BankRRN: result.BankRRN,
-          success: result.success, // Whether the Tx was successful or not.
-          response: result.response, // Details about the success/error
-          message: result.message, // Details about the success/error
           // If this req is successful, we wait for the callback from the
-          // bank else we tell the upstream their req was successful, passing
+          // bank else we tell the upstream their req was not successful, passing
           // the error message from the bank.
-          status: result.success ? "pending" : "success"
-        }};
+          if (!result.success) {
+            return { statusCode: 501, data: {
+                status: "errored",
+                ...result
+            }};
+          } else {
+            return { statusCode: 201, data: {
+              status: "pending",
+              txId: result.SeqNo,
+              BankRRN: result.BankRRN,
+              success: result.success, // Whether the Tx was successful or not.
+              response: result.response, // Details about the success/error
+              message: result.message, // Details about the success/error
+            }};
+          }
       })
-      .catch((error: Error) => ({ statusCode: 503, data: error.message }));
+      .catch((error: Error) => ({ statusCode: 503, data: error }));
 };
