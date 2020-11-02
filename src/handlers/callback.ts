1f85e99b05db694fbb516317651f36450f9b6955
Shaleen Jain <shaleen@jain.sh>
2020-11-02 11:25:31 +0530

    callback: catch unhandled exceptions

--- src/handlers/callback.ts
+++ src/handlers/callback.ts
@@ -1,23 +1,33 @@
 import fetch from "node-fetch";
 
 import config from "../config";
-import { ICallbackClient, ChainlinkClient, getCallbackClient } from "../httpClients";
+import {
+  ICallbackClient,
+  ChainlinkClient,
+  getCallbackClient,
+} from "../httpClients";
 import { Event, TxEvent } from "../event";
 
 export const callbackHandle = async (req: any, res: any) => {
   const chainlink = ChainlinkClient;
   const bank = req.params.bank;
 
-  return getCallbackClient(bank)
-    .processCallback(req.body)
-    .then((result) => {
-
-      chainlink.patchUpdateRun(result.id, result.txstatus )
-        .then(() => Event.collectEvent.emit("stop", result.id))
-        .catch((e) => console.error(e))
-        .finally(() => res.type(result.type).status(result.statusCode).send(result.body));
-    })
-    .catch(({ statusCode, type, body}) => {
-      res.type(type).status(statusCode).send(body);
-    });
+  try {
+    return getCallbackClient(bank)
+      .processCallback(req.body)
+      .then((result) => {
+        chainlink
+          .patchUpdateRun(result.id, result.txstatus)
+          .then(() => Event.collectEvent.emit("stop", result.id))
+          .catch((e) => console.error(e))
+          .finally(() =>
+            res.type(result.type).status(result.statusCode).send(result.body)
+          );
+      })
+      .catch(({ statusCode, type, body }) => {
+        res.type(type).status(statusCode).send(body);
+      });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
